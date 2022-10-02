0c04ea3ae534ca3b92327b93341f8a56d288451d
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2022-10-02 01:46:12 +0100

    Add user login/logout tracking test

--- user_portfolio/tests.py
+++ user_portfolio/tests.py
@@ -1,7 +1,8 @@
-from django.test import TestCase
-from django.contrib.auth.models import User
-from .models import Profile
-
+from multiprocessing.connection import Client
+from django.test import TestCase, Client
+from django.contrib.auth.models import User, AnonymousUser
+from .models import Profile, LoggedInUsers
+from django.contrib.auth import authenticate, login
 
 # Create your tests here.
 class ProfileModelTest( TestCase ):
@@ -25,4 +26,28 @@ class ProfileModelTest( TestCase ):
         self.assertIs( len( profilesSet ) == 1, True )
         profile2 = profilesSet.first()
         self.assertIs( profile2.bio == "Edited", True )
+    
+    def test_login_logout_activity(self):
+        user1 = User.objects.create_user( username="user1", email="user1@gmail.com", password="password1" )
+        user2 = User.objects.create_user( username="user2", email="user2@gmail.com", password="password2" )
+        user3 = User.objects.create_user( username="user3", email="user3@gmail.com", password="password3" )
+
+        c1 = Client()
+        c2 = Client()
+        c3 = Client()
+        
+        self.assertTrue( c1.login( username='user1', password='password1' ) )
+        self.assertTrue( c2.login( username='user2', password='password2' ) )
+        self.assertTrue( c3.login( username='user3', password='password3' ) )
+
+        logged_in = LoggedInUsers.objects.all()
+        self.assertEqual( len(logged_in), 3 )
+        self.assertEqual( set( u.user.username for u in logged_in ), {"user1", "user2", "user3"} )
+        
+        c2.logout()
+
+        logged_in = LoggedInUsers.objects.all()
+        self.assertEqual( len(logged_in), 2 )
+        self.assertEqual( set( u.user.username for u in logged_in ), {"user1", "user3"} )
+
 
